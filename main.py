7641b44fcce3fde7fdca866c7f76aa8baa09793b
Peoni Valle <peonival@gmail.com>
2025-11-17 22:50:18 -0600

    Added left and right for the character when its turning and when its moving

--- main.py
+++ main.py
@@ -23,6 +23,43 @@ image_path = os.path.join(script_dir, 'assets', 'tempbg.png')
 background_image = pygame.image.load(image_path).convert()
 background_image = pygame.transform.scale(background_image, (width, height))
 
+#it'll help the sprites collision for this
+
+class Player(pygame.sprite.Sprite):
+   color = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left" # will shoq the direction of the character
+        self.animation_count = 0  #change the animation frames
+        self.fall_count = 0
+        self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0
+
+    def move(self, dx, dy):
+       self.react.x +- dx
+       self.react.y +- dy
+
+    def move_left(self, vel):
+      self.x_vel = -vel
+      if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+      self.x_vel = vel
+      if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+
+
+
+
 def main(window):
     clock = pygame.time.Clock()
 
