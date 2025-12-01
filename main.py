e1a72de284efa53ac471ac65b785c104a8dd7166
Peoni Valle <peonival@gmail.com>
2025-11-30 19:30:32 -0600

    Addding a path to have access to sprites for the player

--- main.py
+++ main.py
@@ -16,7 +16,24 @@ player_vel = 5
 
 window = pygame.display.set_mode((width, height))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False)for sprite in sprites]
 
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join('assets', dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect =  pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0,0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
 
 script_dir = os.path.dirname(__file__) 
 
